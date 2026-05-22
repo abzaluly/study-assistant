c94eb067a11715bedc37d2f101f3543f14dd4dbe
abzaluly <abzaluly@users.noreply.github.com>
2026-05-22 07:43:58 +0000

    deploy: 8f1b7cb0d96ef550bfff4c855001403aa704ce9c

--- assets/__vite-browser-external-CEfEmZsP.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DIQ8nFZV.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ assets/__vite-browser-external-Dk6Slmzx.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-Bqx_8Fkp.js";var t=e(((e,t)=>{t.exports={}}));export default t();
